7ad72e1486edb2765d59c2a5f263b1932596625e
timon94 <a.bobarikin94@gmail.com>
2016-02-22 09:19:57 +0300

    add video container

--- js/app.routing.js
+++ js/app.routing.js
@@ -3,13 +3,18 @@
     angular.module('sportApp').config(function($routeProvider) {
 
         $routeProvider.when('/home', {
-            templateUrl: 'src/view/broadcasts.view.html',
-            controller: 'broadcastsController',
+            templateUrl: 'src/view/home.view.html',
+            controller: 'profileController',
         });
 
         $routeProvider.when('/profile', {
-            templateUrl: 'src/view/profile.view.html',
+            templateUrl: 'src/view/messages.view.html',
             controller: 'profileController',
+        });
+
+        $routeProvider.when('/broadcasts', {
+            templateUrl: 'src/view/broadcasts.view.html',
+            controller: 'broadcastsController',
         });
 
         $routeProvider.otherwise({redirectTo: '/home'});
--- js/app.service.navbar.js
+++ js/app.service.navbar.js
@@ -1,7 +1,7 @@
 (function() {
 
     angular.module('sportApp').service('TagNavBar', function() {
-        var tagsData = [{tagName: 'Home', tagLink: '#/home'}, {tagName: 'Profile', tagLink: '#/profile'}, {tagName: 'Messages', tagLink: ''}];
+        var tagsData = [{tagName: 'Home', tagLink: '#/home'}, {tagName: 'Profile', tagLink: '#/profile'}, {tagName: ' Broadcasts', tagLink: '#/broadcasts'}];
 
         this.getTags = function() {
             return tagsData;
