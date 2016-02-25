235d9d3db2b7fc5e2aeafdc4254eb29d570cb538
timon94 <a.bobarikin94@gmail.com>
2016-02-25 09:23:44 +0300

    uncaught error

--- src/js/list/list.controller.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('list').controller('listController', function ($scope, Broadcasts) {
-        Broadcasts.getData().then(function (response) {
-            $scope.broadcastsData = response.data;
-        });
-    });
-})();
--- src/module/module.app/app.routing/app.routing.js
+++ src/module/module.app/app.routing/app.routing.js
@@ -3,27 +3,27 @@
     angular.module('sportApp').config(function($routeProvider) {
 
         $routeProvider.when('/home', {
-            templateUrl: 'src/view/home.view.html',
+            templateUrl: 'src/module/module.app/app.view/home.view.html',
             controller: 'profileController',
         });
 
         $routeProvider.when('/profile', {
-            templateUrl: 'src/view/profile.view.html',
-            controller: 'broadcastsController',
+            templateUrl: 'src/module/module.profile/profile.view/profile.view.html',
+            controller: 'profileController',
         });
 
         $routeProvider.when('/broadcasts', {
-            templateUrl: 'src/view/broadcasts.view.html',
+            templateUrl: 'src/module/module.broadcasts/broadcasts.view/broadcasts.view.html',
             controller: 'broadcastsController',
         });
 
         $routeProvider.when('/list', {
-           templateUrl: 'src/view/list.view.html',
+           templateUrl: 'src/module/module.broadcasts/broadcasts.view/list.view.html',
             controller: 'listController',
         });
 
         $routeProvider.when('/list/:listId', {
-            templateUrl: 'src/view/listId.view.html',
+            templateUrl: 'src/module/module.broadcasts/broadcasts.view/listId.view.html',
             controller: 'broadcastController',
         });
 
